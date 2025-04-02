ea7ac4320873dbbf81e936c4e11668e51a26a6bf
Yago Rodrigues <yagostrodrigues@gmail.com>
2025-04-01 21:29:16 -0300

    Add files via upload

---
+++ Aula_01/aula_01_01.py
@@ -0,0 +1,11 @@
+# Programa para calcular as 4 operações matemáticas
+nome = 'Yago Rodrigues'
+idade = 32
+nota = 8.5
+num_01 = int(input ('Digite o primeiro valor: '))
+num_02 = int (input ('Digite o segundo valor: 1'))
+adicao = num_01 + num_02
+print('A soma dos valores é: ', num_01+ num_02)
+print('A subtracao dos valores é: ', num_01 - num_02)
+print('A multiplicaçao dos valores é: ', num_01 * num_02)
+print('A divisao dos valores é: ', num_01/num_02)
